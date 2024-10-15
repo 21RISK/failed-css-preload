1ab81e60d87271b9775f44bfdcc18499dff6e2d5
Alex Bjørlig <alexbjorlig@icloud.com>
2024-10-15 16:47:52 +0200

    1.1.22

--- src/routes/+layout.svelte
+++
@@ -1,8 +0,0 @@
-<!-- src/routes/+layout.svelte -->
-<script>
-    import { updated } from '$app/stores';
-  </script>
-  
-  <main data-sveltekit-reload={$updated ? '' : 'off'}>
-    <slot />
-  </main>
