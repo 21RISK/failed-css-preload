9117696c5ece48a9d19236fc425cb56edb226943
Alex Bjørlig <alexbjorlig@icloud.com>
2024-10-15 16:37:14 +0200

    1.1.19

--- src/routes/foo-bar/+page.svelte
+++ src/routes/foo-bar/+page.svelte
@@ -23,7 +23,7 @@
     Let's add some css on this page, to check pre-fetching of code/data.
     */
     h1 {
-        color: greenyellow;
+        color: blueviolet;
         font-size: large;
     }
 </style>
